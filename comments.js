cf58e40293e50da8cede68698a87e54cabd2acde
Kumatamo-AI <57571432+Kumatamo@users.noreply.github.com>
2023-07-20 23:27:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,23 +1,57 @@
 // create web server
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
-
-// set body parser
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// set view engine
-app.set('view engine', 'pug')
-
-// set static files
-app.use(express.static('public'))
-
-// set routes
-app.use('/', require('./routes/home'))
-app.use('/comments', require('./routes/comments'))
-
-// listen to the port
-app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
-})
+const express = require('express');
+const app = express();
+// parse body of request
+const bodyParser = require('body-parser');
+// connect to database
+const mongoose = require('mongoose');
+// connect to database
+mongoose.connect('mongodb://localhost:27017/comments', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+// create schema
+const commentSchema = new mongoose.Schema({
+  name: String,
+  comment: String
+});
+// create model
+const Comment = mongoose.model('Comment', commentSchema);
+// use body parser
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// use public folder
+app.use(express.static(__dirname + '/public'));
+// use ejs
+app.set('view engine', 'ejs');
+// use routes
+app.get('/', (req, res) => {
+  Comment.find({}, (err, comments) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('comments', {
+        comments: comments
+      });
+    }
+  });
+});
+// post route
+app.post('/comments', (req, res) => {
+  const comment = new Comment({
+    name: req.body.name,
+    comment: req.body.comment
+  });
+  comment.save(err => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+// port to listen
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
